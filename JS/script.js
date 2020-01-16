698aedae595f66133fc345cc327d8c87c333de27
abhishekkolkar <abhishekkolkar41@gmail.com>
2020-01-16 17:44:24 +0530

    js2

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+var name = "Abhishek";
+var nameArray = ["Abhi", "Adi", "Tushar"];
+var arrayOfObjects = [
+  {
+    name: "abhi",
+    city: "pune"
+  },
+  {
+    name: "asd",
+    city: "pune"
+  }
+];
